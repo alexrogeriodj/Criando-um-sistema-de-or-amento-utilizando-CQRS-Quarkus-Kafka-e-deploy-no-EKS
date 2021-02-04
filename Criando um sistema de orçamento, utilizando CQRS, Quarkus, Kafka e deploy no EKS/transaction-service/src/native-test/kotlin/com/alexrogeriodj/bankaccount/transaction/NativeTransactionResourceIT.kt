9ea63e91edd43e8dfdfecd96675d088b9e23eb2c
alexrogeriodj <alexrogeriodj@gmail.com>
2021-02-04 13:15:22 +0800

    Add files via upload

---
+++ Criando um sistema de orçamento, utilizando CQRS, Quarkus, Kafka e deploy no EKS/transaction-service/src/native-test/kotlin/com/alexrogeriodj/bankaccount/transaction/NativeTransactionResourceIT.kt
@@ -0,0 +1,6 @@
+package com.alexrogeriodj.bankaccount.transaction
+
+import io.quarkus.test.junit.NativeImageTest
+
+@NativeImageTest
+open class NativeTransactionResourceIT : TransactionResourceTest()
